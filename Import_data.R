af1d6454f377c886155122ce8dba490254058692
Pedro Martins <pedro.rufino.martins@gmail.com>
2025-10-20 16:07:51 -0300

    few changes

--- Import_data.R
+++ Import_data.R
@@ -1,11 +1,14 @@
 library(readxl)
+library(ggplot2)
 Mestrado_Gabriela_Litter_Bags <- read_excel("Mestrado Gabriela G. de Matos - Litter Bags.xlsx")
 Mestrado_Gabriela_Litter_Bags <- Mestrado_Gabriela_Litter_Bags [,-c(9:11)]
-na.omit(Mestrado_Gabriela_Litter_Bags)
+Mestrado_Gabriela_Litter_Bags <- na.omit(Mestrado_Gabriela_Litter_Bags)
 
 names (Mestrado_Gabriela_Litter_Bags) [c(4:7)] <- c ("galho_g", "folha_g",
                                                      "perda_galho_percentage",
                                                      "perda_folha_percentage")
+
+plot (tempo ~galho_g, data =Mestrado_Gabriela_Litter_Bags)
 
  anova (aov (galho_g ~ cor,
              data = Mestrado_Gabriela_Litter_Bags)
@@ -30,6 +33,7 @@ names (Mestrado_Gabriela_Litter_Bags) [c(4:7)] <- c ("galho_g", "folha_g",
 
 
 is.na (Mestrado_Gabriela_Litter_Bags$galho_g )
+
 
 
  par  (mfrow = c(1,2))
