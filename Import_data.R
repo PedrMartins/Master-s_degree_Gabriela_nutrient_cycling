94b726c90adcef8b6b801ae23d8a2c36fa622e0b
Pedro Martins <pedro.rufino.martins@gmail.com>
2025-10-20 13:58:18 -0300

    start stats anova

--- Import_data.R
+++ Import_data.R
@@ -1,5 +1,40 @@
 library(readxl)
 Mestrado_Gabriela_Litter_Bags <- read_excel("Mestrado Gabriela G. de Matos - Litter Bags.xlsx")
-View(Mestrado_Gabriela_Litter_Bags)
+Mestrado_Gabriela_Litter_Bags <- Mestrado_Gabriela_Litter_Bags [,-c(9:11)]
+na.omit(Mestrado_Gabriela_Litter_Bags)
 
+names (Mestrado_Gabriela_Litter_Bags) [c(4:7)] <- c ("galho_g", "folha_g",
+                                                     "perda_galho_percentage",
+                                                     "perda_folha_percentage")
+
+ anova (aov (galho_g ~ cor,
+             data = Mestrado_Gabriela_Litter_Bags)
+        )
+ anova (aov (folha_g ~ cor,
+             data = Mestrado_Gabriela_Litter_Bags)
+        )
+
+
+ anova (aov (galho_g ~ local,
+             data = Mestrado_Gabriela_Litter_Bags)
+        )
+ anova (aov (folha_g ~ local,data = Mestrado_Gabriela_Litter_Bags)
+        )
+
+ anova (aov (galho_g ~ cor*local,
+             data = Mestrado_Gabriela_Litter_Bags)
+ )
+ anova (aov (folha_g ~ cor*local,
+             data = Mestrado_Gabriela_Litter_Bags)
+ )
+
+
+is.na (Mestrado_Gabriela_Litter_Bags$galho_g )
+
+
+ par  (mfrow = c(1,2))
+ boxplot (folha_g ~tempo
+          , data = Mestrado_Gabriela_Litter_Bags)
+ boxplot (galho_g ~tempo
+          , data = Mestrado_Gabriela_Litter_Bags)
 
