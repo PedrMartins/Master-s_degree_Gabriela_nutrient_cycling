66a056892a1e849a86335048ccfbc156f8d0fd43
Pedro Martins <pedro.rufino.martins@gmail.com>
2025-10-29 10:54:19 -0300

    new plot ANOVA

--- plots_desvio.R
+++ plots_desvio.R
@@ -1,48 +1,5 @@
-############rbase plot##########
-
-# Colors for groups
-cols <- colorRampPalette(c("darkolivegreen3", "skyblue3"))
-
-# Create grouped barplot
-bp <- barplot(
-  height = tapply(MastersGaby$mean,
-                  list(MastersGaby$nivel,
-                       MastersGaby$tempo),
-                  mean),
-  beside = TRUE,
-  col = cols (6),
-  ylim = c(0, max(MastersGaby$mean + MastersGaby$SD) + 10),
-  ylab = "Mean ± SD",
-  main = "Barplot with Error Bars by Site and Time",
-  legend.text = rownames(tapply(MastersGaby$mean, list(MastersGaby$nivel, MastersGaby$tempo), mean)),
-  args.legend = list(x = "topright", bty = "n")
-)
-
-# Add error bars
-arrows(
-  x0 = bp,
-  y0 = MastersGaby$mean,
-  x1 = bp,
-  y1 = MastersGaby$mean + MastersGaby$SD,
-  angle = 90,
-  code = 2,
-  length = 0.05
-)
-arrows(
-  x0 = bp,
-  y0 = MastersGaby$mean,
-  x1 = bp,
-  y1 = MastersGaby$mean - MastersGaby$SD,
-  angle = 90,
-  code = 2,
-  length = 0.05
-)
-
-
 
 ######ggplot #######
-
-library(ggplot2)
 
 ggplot(MastersGaby_folha, aes(x = tempo, y = mean, fill = nivel)) +
   geom_bar(stat = "identity", position = position_dodge(0.9)) +
@@ -58,9 +15,6 @@ ggplot(MastersGaby_folha, aes(x = tempo, y = mean, fill = nivel)) +
   ggsave(filename = "plot_folhas.jpg")
 
 
-
-library(ggplot2)
-
 ggplot(MastersGaby_galho, aes(x = tempo, y = mean, fill = nivel)) +
   geom_bar(stat = "identity", position = position_dodge(0.9)) +
   geom_errorbar(aes(ymin = mean - SD, ymax = mean + SD),
@@ -73,5 +27,59 @@ ggplot(MastersGaby_galho, aes(x = tempo, y = mean, fill = nivel)) +
   theme_minimal() +
   scale_fill_brewer(palette = "Set2") +
   ggsave(filename = "plot_gabys_galho.jpg")
+
+######boxplot ########
+
+boxplot(perda_folha_percentage ~ local * tempo, data = Mestrado_Gabriela_Litter_Bags_folha_s_outlier,
+        main = "Biomass by Site and Time (ANOVA)",
+        xlab = "Group (Site x Time)",
+        ylab = "Perda folha (%)")
+
+# Add mean points
+means <- tapply(Mestrado_Gabriela_Litter_Bags_folha_s_outlier$perda_folha_percentage,
+                interaction(Mestrado_Gabriela_Litter_Bags_folha_s_outlier$local,
+                            Mestrado_Gabriela_Litter_Bags_folha_s_outlier$tempo),
+                mean)
+points(1:length(means), means, pch = 19, col = "red")
+
+######Anova plot########
+
+##folha
+
+summary_df <- Mestrado_Gabriela_Litter_Bags_folha_s_outlier %>%
+  group_by(local, tempo) %>%
+  summarise(mean = mean(perda_folha_percentage),
+            se = sd(perda_folha_percentage) / sqrt(n()), .groups = "drop")
+
+
+ggplot(summary_df, aes(x = tempo, y = mean, fill = local)) +
+  geom_bar(stat = "identity", position = position_dodge(0.9)) +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
+                position = position_dodge(0.9), width = 0.2) +
+  labs(title = "ANOVA: Perda de massa (%) \n por local e tempo \n Folha",
+       x = "Tempo",
+       y = "Média da porcentagem \n de perda de massa ± SE") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set2") +
+  ggsave(filename = "plot_gabys_folha_anova.jpg")
+
+##galho
+
+summary_df <- Mestrado_Gabriela_Litter_Bags_galho_s_outlier %>%
+  group_by(local, tempo) %>%
+  summarise(mean = mean(perda_galho_percentage),
+              se = sd(perda_galho_percentage) / sqrt(n()), .groups = "drop")
+
+
+ggplot(summary_df, aes(x = tempo, y = mean, fill = local)) +
+  geom_bar(stat = "identity", position = position_dodge(0.9)) +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se),
+                position = position_dodge(0.9), width = 0.2) +
+  labs(title = "ANOVA: Perda de massa (%)\n por local e tempo \n Galho",
+       x = "Tempo",
+       y = "Média da porcentagem \n de perda de massa ± SE") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set2") +
+  ggsave(filename = "plot_gabys_galho_anova.jpg")
 
 
